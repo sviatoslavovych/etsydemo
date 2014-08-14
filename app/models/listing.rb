917d1204c15447feb915abc0ee434fb5afc767ba
Igor <nio_08@mail.ru>
2014-08-14 16:43:16 -0700

    updates

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ActiveRecord::Base
   if Rails.env.development?
-    has_attached_file :image, :styles => {:medium => "200x", :thumb => "100x100>" }
+    has_attached_file :image, :styles => {:medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   else
     has_attached_file :image, :styles => {:medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
                       :storage => :dropbox,
